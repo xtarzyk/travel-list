dcf1d8e33d83821d8c95c12595e4f0c6e97463a0
xtarzyk <61456484+xtarzyk@users.noreply.github.com>
2024-01-03 16:17:31 +0100

    added selecting packed items

--- src/Item.jsx
+++ src/Item.jsx
@@ -1,7 +1,8 @@
 /* eslint-disable react/prop-types */
-function Item({ item, onDeleteItem }) {
+function Item({ item, onDeleteItem, onToggleItem }) {
   return (
     <li>
+      <input type="checkbox" value={item.packed} onChange={() => onToggleItem(item.id)}/>
       <span style={item.packed ? { textDecoration: "line-through" } : {}}>
         {item.quantity} {item.description}
       </span>
--- src/PackingList.jsx
+++ src/PackingList.jsx
@@ -1,17 +1,12 @@
 import Item from "./Item";
 import "./index.css";
 
-const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: false },
-];
-
-function PackingList({ items, onDeleteItem }) {
+function PackingList({ items, onDeleteItem, onToggleItem }) {
   return (
     <div className="list">
       <ul>
         {items.map(item => (
-          <Item item={item} key={item.id} onDeleteItem={onDeleteItem} />
+          <Item item={item} key={item.id} onDeleteItem={onDeleteItem} onToggleItem={onToggleItem} />
         ))}
       </ul>
     </div>
