0ea3d03810921b6966f2fff1140c572c01359f92
xtarzyk <61456484+xtarzyk@users.noreply.github.com>
2024-01-03 16:07:33 +0100

    adding and deleting of items

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,24 @@ import Logo from "./Logo";
 import Form from "./Form";
 import PackingList from "./PackingList";
 import Stats from "./Stats";
+import { useState } from 'react';
 
 function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems(items => [...items, item])
+  }
+
+  function handleDeleteItem(id) {
+    setItems(items => items.filter(item => item.id !== id))
+  }
+
   return (
     <div className="app">
       <Logo />
-      <Form />
-      <PackingList />
+      <Form onAddItems={handleAddItems} />
+      <PackingList items={items} onDeleteItem={handleDeleteItem}/>
       <Stats />
     </div>
   );
--- src/PackingList.jsx
+++ src/PackingList.jsx
@@ -6,12 +6,12 @@ const initialItems = [
   { id: 2, description: "Socks", quantity: 12, packed: false },
 ];
 
-function PackingList() {
+function PackingList({ items, onDeleteItem }) {
   return (
     <div className="list">
       <ul>
-        {initialItems.map((item) => (
-          <Item item={item} key={item.id} />
+        {items.map(item => (
+          <Item item={item} key={item.id} onDeleteItem={onDeleteItem} />
         ))}
       </ul>
     </div>
